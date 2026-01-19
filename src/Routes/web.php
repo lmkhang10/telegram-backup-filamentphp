8762938b8b9df08b007a99c070a43eb85a24b66b
barryle <lmkhang89@gmail.com>
2026-01-19 10:14:52 +0700

    Fixing router

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use FieldTechVN\TelegramBackup\Http\Controllers\TelegramBackupController;
 
-Route::middleware([\"web\", \"auth\"])
-    ->prefix(\"telegram-backup\")
-    ->name(\"telegram-backup.\")
+Route::middleware(["web", "auth"])
+    ->prefix("telegram-backup")
+    ->name("telegram-backup.")
     ->group(function () {
-        Route::get(\"/download/{id}\", [TelegramBackupController::class, \"download\"])
-            ->name(\"download\");
+        Route::get("/download/{id}", [TelegramBackupController::class, "download"])
+            ->name("download");
     });
